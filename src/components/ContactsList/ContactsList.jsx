129443affc2f9101defdfcd8f17b4362fb781e3e
Dimch93 <dimch93@ukr.net>
2023-12-10 14:02:48 +0200

    home6-1

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -5,24 +5,25 @@ import { remove } from '../../redux/sliceContact';
 export const ContactsList = ({ listContact }) => {
   const dispatch = useDispatch();
 
-  return listContact.map(cont => {
+  return listContact.map(contacts => {
     const deleteContacts = id => {
       dispatch(remove(id));
     };
     return (
-      <ul key={cont.id} className={css.contacts}>
-        <span>
-          {cont.name}: {cont.number}
-        </span>
-        <button
-          className={css.btn}
-          type="button"
-          onClick={() => {
-            deleteContacts(cont.id);
-          }}
-        >
-          Delete
-        </button>
+      <ul className={css.contacts}>
+        <li key={contacts.id} className={css.list}>
+          <p>{contacts.name}</p>
+          <p>{contacts.number}</p>
+          <button
+            className={css.btn}
+            type="button"
+            onClick={() => {
+              deleteContacts(contacts.id);
+            }}
+          >
+            Delete
+          </button>
+        </li>
       </ul>
     );
   });
